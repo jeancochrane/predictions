dc6e801022ab928c3948b162b7548a125c07a820
Jean Cochrane (Senior Backend Engineer, Sendwave) <jean@jeancochrane.com>
2020-11-11 12:53:38 -0600

    Add sticky note component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,14 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Sticky from '../components/sticky'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Sticky>
+      Drag Me! See how children are passed through to the div!
+    </Sticky>
   </Layout>
 )
 
