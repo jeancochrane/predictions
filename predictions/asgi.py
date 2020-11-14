ac314f453851bd871c5635f90b7594a94b46eb55
Jean Cochrane (Senior Backend Engineer, Sendwave) <jean@jeancochrane.com>
2020-11-13 21:02:42 -0600

    Avoid 'Apps not loaded' error when loading ASGI application

--- predictions/asgi.py
+++ predictions/asgi.py
@@ -9,19 +9,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+import django
 
-from predictions import routing
-
+# Set up Django before defining the application, because otherwise the
+# channels.AuthMiddlewareStack import throws an "apps not loaded" error
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'predictions.settings')
+django.setup()
 
-application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            routing.websocket_urlpatterns
-        )
-    )
-})
+from predictions.routing import application
--- predictions/routing.py
+++ predictions/routing.py
@@ -1,7 +1,15 @@
 from django.urls import re_path
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 
 from . import consumers
 
-websocket_urlpatterns = [
-    re_path(r'ws/predictions/(?P<year>\w+)/$', consumers.PredictionConsumer.as_asgi()),
-]
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter([
+            re_path(r'ws/predictions/(?P<year>\w+)/$', consumers.PredictionConsumer.as_asgi()),
+        ])
+    )
+})
