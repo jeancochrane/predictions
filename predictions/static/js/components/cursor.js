2d4e75dbea52dbf7ebc2df184167ae525174cad5
Jean Cochrane (Senior Backend Engineer, Sendwave) <jean@jeancochrane.com>
2020-12-30 09:11:46 -0600

    Change stroke-width -> strokeWidth in cursor SVG

--- predictions/static/js/components/cursor.js
+++ predictions/static/js/components/cursor.js
@@ -16,7 +16,7 @@ const Cursor = ({x, y, username, color}) => {
       <polygon
         fill={color ? color : "#656565"}
         stroke="grey"
-        stroke-width="15"
+        strokeWidth="15"
         points="1283.1857,1127.3097 1406.1421,1077.6322 1314.2406,850.1678 1463.913,852.7823 1093.4828,480.8547
       1085.4374,1005.6964 1191.2842,899.8454"
       />
